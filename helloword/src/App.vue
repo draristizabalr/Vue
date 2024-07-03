b4fb669c55c3090e5033de9ad1478cc9a8472143
david.aristizabal <david.aristizabal@kiplan.io>
2024-07-03 17:04:36 -0500

    First commit

---
+++ helloword/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <CounterComposition />
+</template>
+
+<script lang="ts" setup>
+import CounterComposition from './components/CounterComposition.vue';
+</script>
+
+<style>
+h1 {
+  color: green;
+}
+</style>
---
+++ helloword/src/components/PostCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="post-card">
+    <h3>Titulo</h3>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur veritatis, nesciunt deserunt rerum quos et, rem maxime voluptatum exercitationem dignissimos sed commodi quia quis, aspernatur labore. Suscipit architecto tempora recusandae!</p>
+    <PostButton />
+  </div>
+</template>
+<script lang="ts" setup>
+import PostButton from './PostButton.vue';
+
+</script>
+<style>
+  .post-card {
+    border-radius: 15px;
+    padding: 10px;
+    margin: 5px;
+    border: 1px solid #ccc;
+  }
+</style>
