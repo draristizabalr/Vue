715f70b18f9d74ef86f03d82a860c0d3eba2b933
david.aristizabal <david.aristizabal@kiplan.io>
2024-07-04 20:20:00 -0500

    V-binds

--- helloword/src/App.vue
+++ helloword/src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <!-- <ExampleSelective /> -->
-   <ExampleFor />
+   <!-- <ExampleFor /> -->
+  <ModelForm />
 </template>
 
 <script lang="ts" setup>
 // import CounterComposition from './components/CounterComposition.vue';
 // import ExampleSelective from './components/ExampleSelective.vue'
-import ExampleFor from './components/ExampleFor.vue'
+// import ExampleFor from './components/ExampleFor.vue'
+import ModelForm from './components/ModelForm.vue'
+
 </script>
 
 <style>
---
+++ helloword/src/components/ModelForm.vue
@@ -0,0 +1,36 @@
+<template>
+  <form class="form">
+    <label for="name">Escribe tu nombre</label>
+    <input type="text" name="name" id="name" v-model="inputText">
+    <label for="agree">
+      <input type="checkbox" v-model="agree" name="agree" id="agree">
+      Acepto terminos y condiciones
+    </label>
+    <label for="country">Selecciona tu pais de origen</label>
+    <select name="country" id="country" v-model="selection">
+      <option v-for="country in countries" :key="country" :value="country">{{ country }}</option>
+    </select>
+  </form>
+  <h3 v-if="inputText && selection" :style="{color: agree ? 'green' : 'red'}">{{ `Hola ${inputText} de ${selection}` }}</h3>
+</template>
+
+<script lang="ts" setup>
+import { ref, Ref } from 'vue'
+
+const countries:Array<string> = ['Spain', 'Portugal', 'France', 'USA', 'Germany', 'Colombia']
+
+let inputText:Ref<string> = ref(''),
+    agree:Ref<boolean> = ref(false),
+    selection: Ref<string> = ref('')
+
+
+</script>
+
+<style scope>
+.form {
+  width: 20%;
+  display: flex;
+  flex-direction: column;
+  gap: 0.5em;
+}
+</style>
